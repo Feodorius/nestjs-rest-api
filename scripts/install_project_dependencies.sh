72c1060dc989cd50b4ee810dd0d4e6e0142d7536
Fedor Kaputsky <fkaputsky@mail.ru>
2026-04-22 22:38:41 +0200

    update project dependencies

--- scripts/install_project_dependencies.sh
+++ scripts/install_project_dependencies.sh
@@ -16,10 +16,10 @@ npm install --legacy-peer-deps
 # Use AWS Secrets service for setting env variables in prod;
 file_location=./.env
 cat >$file_location <<EOF
-ENDPOINT=""
+ENDPOINT="database-1.cveg0o4usup9.eu-north-1.rds.amazonaws.com"
 USER_NAME="postgres"
 DB_PORT="5432"
-PASSWORD=""
+PASSWORD="YourPassword123"
 TABLE_SCHEMA_AUTOUPDATE="false"
 DEVELOPMENT="false"
 PORT="3000"
